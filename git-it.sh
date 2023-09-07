01e691c1da78d5588dfa4feb21cb3ead00cadc20
Maruf Sarker <maruf.sarker@gmail.com>
2023-09-07 13:55:50 +0600

    Thu Sep  7 01:55:50 PM +06 2023

--- git-it.sh
+++ git-it.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/bash
 
-
 if [[ ! -d ".git" ]]; then
     git init
 fi
-
 if [[ "$(git config --get user.name)" == "" ]]; then
     git config user.name "User"
 fi
@@ -12,7 +10,8 @@ if [[ "$(git config --get user.email)" == "" ]]; then
     git config user.email "user@localhost"
 fi
 
-git status && \
-git add . && \
-git commit -m "$(date)" && \
 git status
+git add . && git commit -m "$(date)"
+git status
+
+# git reset --hard && git clean -fdx
